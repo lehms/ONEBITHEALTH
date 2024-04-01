6fc67d317af2ef40581ee24553ac36df4a8d185c
lehms <leticia.silva2096@etec.sp.gov.br>
2024-04-01 12:03:36 -0300

    Primeiro Comit 3b3 Lado A

---
+++ src/components/title/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+export default function Title(){
+    return(
+        <View>
+            <Text>ONEBITHEALTH</Text>
+            <Text>01/04/2024 3B3 Lado A</Text>
+        </View>
+    );
+}
